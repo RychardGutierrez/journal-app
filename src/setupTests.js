5dd3b2fc0f8dd9cc7d9d536d55d69d2d81809c9c
Richard Gutierrez Condori <rychard.gutierrez@gmail.com>
2022-05-10 23:09:10 -0400

    Clean project and preparer base

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,16 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+// import "@testing-library/jest-dom";
+import Enzyme from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import { createSerializer } from 'enzyme-to-json';
+
+Enzyme.configure({ adapter: new Adapter() });
+expect.addSnapshotSerializer(createSerializer({ mode: 'deep' }));
+
+window.setImmediate = window.setTimeout;
+window.clearImmediate = window.clearTimeout;
+
+const mockScroll = () => {};
+Object.defineProperty(window, 'scrollTo', {
+  value: mockScroll,
+  writable: true,
+});
